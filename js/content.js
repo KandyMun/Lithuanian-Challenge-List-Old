48e2ece6df2b486b6ffb8a1c8732ab237ecb1aa3
feiiit <lagzinsta@gmail.com>
2025-08-30 22:47:27 +0300

    tiesiog ikeliu is beta branch content.js

--- js/content.js
+++ js/content.js
@@ -174,10 +174,10 @@ export async function fetchLeaderboard() {
     // Wrap in extra Object containing the user and total score
     const res = Object.entries(player).map(([user, scores]) => {
         const { verifiedLevels, completedLevels, packsComplete} = scores;
-        const packBonus = packsComplete.length * 20;
         const total = [verifiedLevels, completedLevels]
             .flat()
             .reduce((prev, cur) => prev + cur.score, 0);
+             const packBonus = packsComplete.length * 20;
              const hardest = [verifiedLevels, completedLevels]
            .flat()
         .reduce(function(prev, cur) {
