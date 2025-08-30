9d278fec22c38373320f380a96704fc9dcaf4afc
feiiit <lagzinsta@gmail.com>
2025-08-30 23:30:16 +0300

    fixed kad points is pakeliu rodytu tiesiai po apacia points

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -65,7 +65,7 @@ export default {
                             <div class="extra" style="width:25%">
                                 <h2 style="padding:5px; width:100%">Taškai</h2>
                                 <div class="levels">
-                                    <div class="level-display" style="font-size:1.4em;">{{ entry.total}}</div>
+                                    <div class="level-display" style="font-size:1.4em;">{{ (entry.total).toFixed(2)}}</div>
                                     <br> </br>
                                     <div class="level-display" style="font-size:0.9em; opacity:0.7;"> ({{ entry.packsComplete.length * 20 }} taškų iš pakelių)
                                         </div>
