2c143bc83787845ea780d4055443366ce87629bb
feiiit <lagzinsta@gmail.com>
2026-01-20 16:38:44 +0200

    nu plyz

--- js/pages/ListPacks.js
+++ js/pages/ListPacks.js
@@ -1,9 +1,10 @@
-import Spinner from "../components/Spinner.js";
-import LevelAuthors from "../components/List/LevelAuthors.js";
 import { fetchPacks, fetchPackLevels } from "../content.js";
 import { getFontColour, embed } from "../util.js";
 import { score } from "../score.js";
 import { store } from "../main.js";
+
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
 
 export default {
     components: {
@@ -11,137 +12,125 @@ export default {
         LevelAuthors,
     },
     template: `
-        <main>
-            <div v-if="loading" class="spinner">
-                <Spinner></Spinner>
+    <main v-if="loading">
+        <Spinner></Spinner>
+    </main>
+    <main v-else class="pack-list">
+        <h1 class="pack-title">PAKELIAI</h1>
+
+        <div class="packs-grid">
+            <div 
+                class="pack-card" 
+                v-for="(pack, packIndex) in packs" 
+                :key="pack.name"
+                :style="{ backgroundImage: 'url(' + pack.image + ')' }"
+            >
+                <div class="pack-name">{{ pack.name }}</div>
+                <div class="levels">
+                    <button 
+                        v-for="(level, levelIndex) in pack.levels" 
+                        :key="level[1] || level.level?.id"
+                        :class="{ active: selectedLevelIndex === levelIndex && selectedPackIndex === packIndex }"
+                        @click="selectLevel(packIndex, levelIndex)"
+                    >
+                        {{ level[0]?.level?.name || \`Error (\${level[1]}.json)\` }}
+                    </button>
+                </div>
             </div>
+        </div>
 
-            <div v-else class="pack-list">
-                <!-- Title -->
-                <div class="pack-title">PAKELIAI</div>
+        <div class="level-container" v-if="selectedPackLevels.length && selectedLevelIndex !== null">
+            <div v-if="selectedPackLevels[selectedLevelIndex]?.[0]">
+                <h1>{{ selectedPackLevels[selectedLevelIndex][0].level.name }}</h1>
 
-                <!-- Packs Grid -->
-                <div class="packs-grid">
-                    <div v-for="(pack, i) in packs" :key="i" class="pack-card">
-                        <div class="pack-name">{{ pack.name }}</div>
-                        <div class="levels">
-                            <button 
-                                v-for="(level, j) in pack.levels" 
-                                :key="j"
-                                @click="selectLevel(i, j)"
-                                :class="{ active: selectedPack === i && selectedLevel === j }">
-                                {{ level[0]?.level.name || \`Error (\${level[1]}.json)\` }}
-                            </button>
-                        </div>
+                <LevelAuthors 
+                    :author="selectedPackLevels[selectedLevelIndex][0].level.author" 
+                    :creators="selectedPackLevels[selectedLevelIndex][0].level.creators" 
+                    :verifier="selectedPackLevels[selectedLevelIndex][0].level.verifier"
+                ></LevelAuthors>
+
+                <div class="packs">
+                    <div 
+                        v-for="pack in selectedPackLevels[selectedLevelIndex][0].level.packs" 
+                        class="tag" 
+                        :style="{ background: pack.colour, color: getFontColour(pack.colour) }"
+                    >
+                        {{ pack.name }}
                     </div>
                 </div>
 
-                <!-- Level Info Section -->
-                <div class="level-container" v-if="currentLevel">
-                    <h1>{{ currentLevel.level.name }}</h1>
-                    <LevelAuthors 
-                        :author="currentLevel.level.author" 
-                        :creators="currentLevel.level.creators" 
-                        :verifier="currentLevel.level.verifier">
-                    </LevelAuthors>
+                <iframe class="video" :src="video" frameborder="0"></iframe>
 
-                    <!-- Level Packs Tags -->
-                    <div class="packs">
-                        <div 
-                            v-for="pack in currentLevel.level.packs" 
-                            class="tag" 
-                            :style="{ background: pack.colour, color: getFontColour(pack.colour) }">
-                            {{ pack.name }}
-                        </div>
-                    </div>
+                <ul class="stats">
+                    <li>
+                        <div class="type-title-sm">Lygio ID</div>
+                        <p>{{ selectedPackLevels[selectedLevelIndex][0].level.id }}</p>
+                    </li>
+                </ul>
 
-                    <!-- Video / Showcase -->
-                    <iframe class="video" :src="video" frameborder="0"></iframe>
-
-                    <!-- Stats -->
-                    <ul class="stats">
-                        <li>
-                            <div class="type-title-sm">Lygio ID</div>
-                            <p>{{ currentLevel.level.id }}</p>
-                        </li>
-                    </ul>
-
-                    <!-- Rekordai / Records -->
-                    <h2>Rekordai</h2>
-                    <table class="records">
-                        <tr v-for="record in currentLevel.records" class="record">
-                            <td class="enjoyment">
-                                <p>100%</p>
-                            </td>
-                            <td class="user">
-                                <a :href="record.link" target="_blank">{{ record.user }}</a>
-                            </td>
-                            <td class="mobile">
-                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store?.dark ? '-dark' : ''}.svg\`" alt="Mobile">
-                            </td>
-                        </tr>
-                    </table>
-
-                    <!-- Errors -->
-                    <div class="errors" v-if="errors.length">
-                        <p class="error" v-for="error of errors">{{ error }}</p>
-                    </div>
-                </div>
+                <h2>Rekordai</h2>
+                <table class="records">
+                    <tr v-for="record in selectedPackLevels[selectedLevelIndex][0].records" class="record">
+                        <td class="enjoyment">
+                            <p>100%</p>
+                        </td>
+                        <td class="user">
+                            <a :href="record.link" target="_blank">{{ record.user }}</a>
+                        </td>
+                        <td class="mobile">
+                            <img v-if="record.mobile" :src="'/assets/phone-landscape' + (store?.dark ? '-dark' : '') + '.svg'" alt="Mobile">
+                        </td>
+                    </tr>
+                </table>
             </div>
-        </main>
+        </div>
+    </main>
     `,
     data: () => ({
         store,
         packs: [],
-        errors: [],
-        selectedPack: 0,
-        selectedLevel: 0,
+        selectedPackIndex: 0,
+        selectedLevelIndex: 0,
+        selectedPackLevels: [],
         loading: true,
+        errors: [],
     }),
     computed: {
-        currentLevel() {
-            if (!this.packs[this.selectedPack]?.levels) return null;
-            return this.packs[this.selectedPack].levels[this.selectedLevel][0];
-        },
         video() {
-            if (!this.currentLevel) return "";
-            const level = this.currentLevel.level;
-            return embed(level.showcase || level.verification);
+            const lvl = this.selectedPackLevels[this.selectedLevelIndex]?.[0]?.level;
+            if (!lvl) return '';
+            return embed(lvl.showcase ? lvl.showcase : lvl.verification);
         },
     },
     async mounted() {
         try {
-            const fetchedPacks = await fetchPacks();
+            this.packs = await fetchPacks();
 
-            // Fetch levels for each pack
-            const packsWithLevels = await Promise.all(
-                fetchedPacks.map(async pack => ({
-                    ...pack,
-                    levels: await fetchPackLevels(pack.name)
-                }))
-            );
-
-            this.packs = packsWithLevels;
-
+            // Fetch levels for the first pack by default
+            if (this.packs.length) {
+                this.selectedPackLevels = await fetchPackLevels(this.packs[this.selectedPackIndex].name);
+            }
         } catch (err) {
-            this.errors.push("Nepavyko pakrauti sąrašo. Pabandykite po kelių minučių.");
-            console.error(err);
+            this.errors.push("Nepavyko pakrauti sąrašo.");
+        } finally {
+            this.loading = false;
         }
-
-        this.loading = false;
     },
     methods: {
-        selectLevel(packIndex, levelIndex) {
-            this.selectedPack = packIndex;
-            this.selectedLevel = levelIndex;
+        async selectLevel(packIndex, levelIndex) {
+            this.selectedPackIndex = packIndex;
+            this.selectedLevelIndex = levelIndex;
 
-            // Reset errors
-            this.errors = [];
+            const packName = this.packs[packIndex].name;
+            this.selectedPackLevels = await fetchPackLevels(packName);
 
-            const level = this.packs[packIndex].levels[levelIndex];
-            if (!level[0]) {
-                this.errors.push(`Nepavyko pakrauti lygio. (${level[1]}.json)`);
-            }
+            // Error handling
+            this.errors.length = 0;
+            this.errors.push(
+                ...this.selectedPackLevels
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => `Nepavyko pakrauti lygio. (${err}.json)`)
+            );
         },
         getFontColour,
         embed,
